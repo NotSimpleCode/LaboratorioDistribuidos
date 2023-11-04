625e2b9dbfda1f32eb571261c5303d1a603327f6
JorgeMz <jorge.martinez09@uptc.edu.co>
2023-11-04 18:01:15 -0500

    deletion of all different files to connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/users_routers.js";
-import rolesRoutes from "./routes/roles_routers_routes.js";
-import typeDocumentsRoutes from "./routes/type_document_routes.js";
 import userRolesRoutes from "./routes/roles_users_routes.js";
 
 const app = express();
@@ -11,9 +8,6 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api', userRoutes);
-app.use('/api', rolesRoutes);
-app.use('/api', typeDocumentsRoutes);
 app.use('/api', userRolesRoutes);
 
 app.listen(3000)
