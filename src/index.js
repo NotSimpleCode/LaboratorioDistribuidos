38c513ed59e045d239416963537d59793657a852
JorgeMz <jorge.martinez09@uptc.edu.co>
2023-11-04 18:42:45 -0500

    add command start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/users_routers.js";
 import rolesRoutes from "./routes/roles_routers_routes.js";
-import typeDocumentsRoutes from "./routes/type_document_routes.js";
-import userRolesRoutes from "./routes/roles_users_routes.js";
 
 const app = express();
 
