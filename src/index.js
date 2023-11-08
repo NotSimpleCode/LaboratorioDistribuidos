a73a635565282212ca564a5986364ac21bfdb17f
JorgeMz <jorge.martinez09@uptc.edu.co>
2023-11-08 17:20:36 -0500

    add route user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import rolesRoutes from "./routes/roles_routers_routes.js";
+import userRoutes from "./routes/users_routers.js";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', rolesRoutes);
+app.use('/api', userRoutes);
 
 app.listen(process.env.PORT || 3000);
