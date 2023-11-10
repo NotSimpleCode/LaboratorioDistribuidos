49adc774509a28059f3cc07d7d10ab203c9e1ff8
JorgeMz <jorge.martinez09@uptc.edu.co>
2023-11-10 12:50:19 -0500

    remove route users

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import rolesRoutes from "./routes/roles_routers_routes.js";
-import userRoutes from "./routes/users_routers.js";
 
 const app = express();
 
@@ -10,6 +9,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', rolesRoutes);
-app.use('/api', userRoutes);
 
 app.listen(process.env.PORT || 3000);
