843a31d15ec97f241b50a9f8b1e5e2b2353663ee
NotSimpleCode <onejobsdiego@gmail.com>
2023-11-12 19:35:13 -0500

    fix index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import rolesRoutes from "./routes/roles_routers_routes.js";
+import tiposDocumentos from "./routes/type_document_routes.js";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', rolesRoutes);
+app.use('/api', tiposDocumentos);
 
 app.listen(process.env.PORT || 3000);
